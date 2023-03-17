0ab02b2b97be59c97523824d2970890978f8cd4a
nlisgo <n.lisgo@elifesciences.org>
2023-03-17 14:06:53 +0000

    adjust script to use enhanced-preprints-biorxiv-xslt docker image

--- scripts/extract_mecas.sh
+++ scripts/extract_mecas.sh
@@ -4,15 +4,10 @@ set -e
 # This requires xmllint and macos/bsd sed
 # pass the directory of mecas as first param, and output directory as second
 # example run:
-# ./scripts/extract_mecas.sh mecas/ data/
-
-NODE_SCRIPT=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/fix_biorxiv_xml.js
-echo $NODE_SCRIPT
+# ./scripts/extract_mecas.sh incoming/ data/
 
 for file in $1/*; do
-    tmpDir="/tmp/mecaunzip"
-    rm -R $tmpDir || true
-
+    tmpDir=$(mktemp -d)
     echo $tmpDir
 
     echo "extracting $file..."
@@ -25,31 +20,31 @@ for file in $1/*; do
     echo -n "getting doi from $tmpDir/$xmlFile ... "
     doi=$(cat $tmpDir/$xmlFile | sed 's/xmlns=".*"//g' | xmllint -xpath 'string(/article/front/article-meta/article-id)' -)
     echo "'$doi'."
+    doiPrefix="${doi%%\/*}"
+    doiSuffix="${doi#*\/}"
 
     outputDir="$2/$doi"
     id=$(basename $outputDir)
     uuid=$(basename -s .meca $file)
-
 
     echo "creating $outputDir"
     mkdir -p "$outputDir"
 
     echo "$uuid" > "$outputDir/source.txt"
 
-    echo "cp $tmpDir/$xmlFile to $outputDir/$id.xml..."
-    cp "$tmpDir/$xmlFile" "$outputDir/$id.xml"
-
-    echo "and correct some encoda XML issues..."
-    # sed -i '' 's|string-name>|name>|g' "$outputDir/$id.xml"  # string-name -> name
-    # sed -i '' -E 's|<label>(.*)</label><title>|<title><label>\1</label> |g' "$outputDir/$id.xml" # <label>1</label><title> -> <title><label>1</label>
-    # sed -i '' 's|^<label>([[:digit:]\.]*)</label>||g' "$outputDir/$id.xml" # <label>1</label>\n -> *delete it*
-    # sed -i '' 's|</table-wrap>|</fig>|g' "$outputDir/$id.xml"  # table-wrap -> figure
-    # sed -i '' 's|<table-wrap|<fig|g' "$outputDir/$id.xml"  # table-wrap -> figure
-    node $NODE_SCRIPT "$outputDir/$id.xml"
+    if [ "$doiPrefix" = "10.1101" ]; then
+        echo "correct some bioRxiv/Encoda XML issues and store $outputDir/$id.xml..."
+        cat "$tmpDir/$xmlFile" | docker run --rm -i ghcr.io/elifesciences/enhanced-preprints-biorxiv-xslt:latest /app/scripts/transform.sh --doi $doiSuffix > "$outputDir/$id.xml"
+        echo "transform.sh --doi $doiSuffix successfully run"
+    else
+        echo "Skipping XML correction for DOI prefix $doiPrefix"
+        cp "$tmpDir/$xmlFile" "$outputDir/$id.xml"
+    fi
 
     echo "copy all tiff content to $outputDir..."
     cp $tmpDir/content/*.tif "$outputDir/" || true
     cp $tmpDir/content/*.gif "$outputDir/" || true
+    cp $tmpDir/content/*.jpg "$outputDir/" || true
 
     echo "cleaning up..."
     rm -R $tmpDir
