569028292594432797c73a3928ea82b00765142e
nlisgo <n.lisgo@elifesciences.org>
2023-03-24 10:27:59 +0000

    Introduce pre sync checks on preserve pdfs

--- scripts/preserve_pdfs.sh
+++ scripts/preserve_pdfs.sh
@@ -11,5 +11,10 @@ DEFAULT_S3_BUCKET="s3://prod-elife-epp-pdf/data" # default s3://prod-elife-epp-p
 S3_BUCKET="${1:-${DEFAULT_S3_BUCKET}}"
 DATA_FOLDER="$(realpath ${2:-${PARENT_DIR}/data})" # default ./data
 
+if [ "$S3_BUCKET" == "$DEFAULT_S3_BUCKET" ]; then
+    echo "Performing pre-sync checks before syncing with prod pdf s3"
+	${SCRIPT_DIR}/pre_sync_check.sh 2>&1
+fi
+
 echo "sync PDFs from ${DATA_FOLDER} to ${S3_BUCKET}"
 aws s3 sync ${DATA_FOLDER} ${S3_BUCKET} --exclude "*" --include "*.pdf" --delete
--- scripts/sync_data.sh
+++ scripts/sync_data.sh
@@ -12,9 +12,10 @@ S3_BUCKET="s3://${PROD_OR_STAGING}-elife-epp-data/data"
 DATA_FOLDER="$(realpath ${2:-${PARENT_DIR}/data})" # default ./data
 
 if [ "$PROD_OR_STAGING" == "prod" ]; then
-	${SCRIPT_DIR}/pre_sync_check.sh 2>&1
-
     ${SCRIPT_DIR}/preserve_pdfs.sh 2>&1
+
+    echo "Performing pre-sync checks before syncing with prod data s3"
+	${SCRIPT_DIR}/pre_sync_check.sh 2>&1
 fi
 
 echo "sync ${DATA_FOLDER} to ${S3_BUCKET}"
