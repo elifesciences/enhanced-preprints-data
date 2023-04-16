5dede381e978b63dfc0e86d359b89b16dfeff840
Nathan Lisgo <n.lisgo@elifesciences.org>
2023-04-16 21:08:54 +0100

    Allow data_dir to be parameter

--- scripts/fetch_meca_archives.sh
+++ scripts/fetch_meca_archives.sh
@@ -2,9 +2,9 @@
 set -e
 
 # This requires jq
-# pass all or new as first param, the output directory as second, manuscripts text file as third, and meca lookup file as fourth
+# pass all or new as first param, the output directory as second, data directory as third, manuscripts text file as fourth and meca lookup file as fifth
 # example run:
-# ./scripts/fetch_meca_archives.sh [all_or_new] [output_dir] [manuscripts_txt] [meca_lookup]
+# ./scripts/fetch_meca_archives.sh [all_or_new] [output_dir] [data_dir] [manuscripts_txt] [meca_lookup]
 
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 PARENT_DIR="$(dirname "${SCRIPT_DIR}")"
@@ -17,8 +17,11 @@ if [[ "$all_or_new" != "all" && "$all_or_new" != "new" ]]; then
 fi
 
 output_dir="$(realpath ${2-${PARENT_DIR}/incoming})" # default ./incoming
-manuscripts_txt="$(realpath ${3-${PARENT_DIR}/manuscripts.txt})" # default ./manuscripts.txt
-meca_lookup="$(realpath ${4-${PARENT_DIR}/meca-lookup.txt})" # default ./meca-lookup.txt
+data_dir="$(realpath ${3-${PARENT_DIR}/data})" # default ./data
+manuscripts_txt="$(realpath ${4-${PARENT_DIR}/manuscripts.txt})" # default ./manuscripts.txt
+meca_lookup="$(realpath ${5-${PARENT_DIR}/meca-lookup.txt})" # default ./meca-lookup.txt
+
+mkdir -p $data_dir
 
 echo "Retrieving ${all_or_new} manuscripts in ${manuscripts_txt}"
 
@@ -27,7 +30,7 @@ while read -r line; do
     dois=($(echo $line | cut -d' ' -f2 | tr ',' ' '))
     for i in "${!dois[@]}"; do
         version=$(( ${i} + 1 ))
-        if [[ "$all_or_new" = "all" || ! -d "${PARENT_DIR}/data/${manuscript_id}/v${version}" ]]; then
+        if [[ "$all_or_new" = "all" || ! -d "${data_dir}/${manuscript_id}/v${version}" ]]; then
             $SCRIPT_DIR/fetch_meca_archive.sh "${dois[$i]}" $output_dir $manuscript_id $version $meca_lookup
         fi
     done
